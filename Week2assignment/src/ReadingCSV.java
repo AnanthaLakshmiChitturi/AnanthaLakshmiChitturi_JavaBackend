0885189ffa609ef7527d4b4bf1183a10b54fae29
LakshmiChitturi <technicalstuff2023@gmail.com>
2023-06-12 14:32:07 -0500

    Readinga and sorting CSV file

--- Week2assignment/src/ReadingCSV.java
+++ Week2assignment/src/ReadingCSV.java
@@ -10,15 +10,18 @@ public class ReadingCSV {
     {
         BufferedReader br;
         br = new BufferedReader(new FileReader( "C:\\Users\\laksh\\Desktop\\BackEnd\\AnanthaLakshmiChitturi_JavaBackend\\Week2assignment\\src\\names.csv") );
-        ArrayList<Person> people = new ArrayList<>();
+        ArrayList<Person> people = new ArrayList<Person>();
         String currentLine;
         while((currentLine=br.readLine())!=null)
         {
             String [] detailed = currentLine.split(",");
+            //System.out.println(detailed[0]);
             if(detailed[0].equals("\"\""))
                 continue;
             String firstName=detailed[1].substring(1, detailed[1].length()-1);
+            //System.out.println(firstName);
             String lastName=detailed[2].substring(1, detailed[2].length()-1);
+            //System.out.println(lastName);
             people.add(new Person(firstName,lastName));
 
         }
